7e6519a60eec959c5c5a27a408ddca102839d162
Shriram Dawange <shriramdawange.dev@gmail.com>
2026-08-18 21:33:09 +0530

    test: expand accounting invariants

--- test/accounting.test.ts
+++ test/accounting.test.ts
@@ -2,23 +2,23 @@ import test from 'node:test';
 import assert from 'node:assert/strict';
 import { validateJournal } from '../src/accounting.js';
 
-test('balanced journal passes', () => {
-  assert.doesNotThrow(() => validateJournal([
-    {accountId:'00000000-0000-0000-0000-000000000001', debit:100, credit:0},
-    {accountId:'00000000-0000-0000-0000-000000000002', debit:0, credit:100}
-  ]));
-});
-
-test('unbalanced journal fails', () => {
-  assert.throws(() => validateJournal([
-    {accountId:'00000000-0000-0000-0000-000000000001', debit:100, credit:0},
-    {accountId:'00000000-0000-0000-0000-000000000002', debit:0, credit:99}
-  ]), /unbalanced/);
-});
-
-test('mixed debit and credit on one line fails', () => {
-  assert.throws(() => validateJournal([
-    {accountId:'00000000-0000-0000-0000-000000000001', debit:100, credit:10},
-    {accountId:'00000000-0000-0000-0000-000000000002', debit:0, credit:90}
-  ]));
-});
+test('balanced journal passes',()=>assert.doesNotThrow(()=>validateJournal([
+ {accountId:'00000000-0000-0000-0000-000000000001',debit:100,credit:0},
+ {accountId:'00000000-0000-0000-0000-000000000002',debit:0,credit:100}
+])));
+test('unbalanced journal fails',()=>assert.throws(()=>validateJournal([
+ {accountId:'00000000-0000-0000-0000-000000000001',debit:100,credit:0},
+ {accountId:'00000000-0000-0000-0000-000000000002',debit:0,credit:99}
+]),/unbalanced/));
+test('mixed debit and credit on one line fails',()=>assert.throws(()=>validateJournal([
+ {accountId:'00000000-0000-0000-0000-000000000001',debit:100,credit:10},
+ {accountId:'00000000-0000-0000-0000-000000000002',debit:0,credit:90}
+])));
+test('zero-value line fails',()=>assert.throws(()=>validateJournal([
+ {accountId:'00000000-0000-0000-0000-000000000001',debit:0,credit:0},
+ {accountId:'00000000-0000-0000-0000-000000000002',debit:100,credit:100}
+])));
+test('fractional paise are rejected by cent balancing',()=>assert.throws(()=>validateJournal([
+ {accountId:'00000000-0000-0000-0000-000000000001',debit:10.001,credit:0},
+ {accountId:'00000000-0000-0000-0000-000000000002',debit:0,credit:10}
+]),/unbalanced/));
